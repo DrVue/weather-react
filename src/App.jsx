ef29c9ae1861a32a24df1d8ae0edbb0fc1d8dcb2
ivansey <van.secret2015@gmail.com>
2021-11-01 18:59:33 +0300

    Add custom MUI theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,27 @@ import Header from "./Components/Header";
 import MainPage from "./MainPage";
 import LandingPage from "./LandingPage";
 
+import {createTheme, ThemeProvider} from "@mui/material";
+
+const theme = createTheme({
+	palette: {
+		white: {
+			main: "#fff",
+		},
+		black: {
+			main: "#000",
+		},
+	},
+});
+
 function App() {
-	return <BrowserRouter>
-		<Header/>
-		<Route path="/" exact component={LandingPage}/>
-		<Route path="/:city" exact component={MainPage}/>
-    </BrowserRouter>
+	return <ThemeProvider theme={theme}>
+		<BrowserRouter>
+			<Header/>
+			<Route path="/" exact component={LandingPage}/>
+			<Route path="/:city" exact component={MainPage}/>
+		</BrowserRouter>
+	</ThemeProvider>
 }
 
 export default App;
