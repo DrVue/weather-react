a85e97e7b18830d12475f0d768f89bb41258f9a8
ivansey <van.secret2015@gmail.com>
2021-11-01 19:00:03 +0300

    Re-style and adding button in LandingPage

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import {Box, Container, Typography} from "@mui/material";
+import {withRouter} from "react-router-dom";
+import {Box, Container, Typography, Button} from "@mui/material";
 
-function LandingPage () {
+function LandingPage(props) {
 	return <div>
 		<Box sx={{backgroundColor: "primary.main"}}>
-			<Container>
-				<Typography variant="h1" component="h3">Погода</Typography>
-				<Typography variant="h2" component="h5">Мой личный проект на React с использованием Node сервера и API</Typography>
+			<Container sx={{textAlign: "center"}}>
+				<br/>
+				<Typography color="white.main" variant="h1" component="h1">Погода</Typography>
+				<br/>
+				<Typography color="white.main" variant="h4" component="h2">Мой личный проект на React с использованием Node
+					сервера и API</Typography>
+				<br/>
+				<Button variant="outlined" color="white" fullWidth onClick={() => props.history.push("/Moscow")}>Погода в Москве</Button>
+				<br/>
+				<br/>
 			</Container>
 		</Box>
 	</div>
 }
 
-export default LandingPage;
+export default withRouter(LandingPage);
